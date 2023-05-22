5061af6e30da197a8ae420f32dcf70fde59cc5cc
= <=>
2023-05-22 20:17:15 +0700

    update

--- src/pages/order/List_order.jsx
+++ src/pages/order/List_order.jsx
@@ -1,2 +1,18 @@
+import "./order.scss"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import Datatable_order from "../../components/datatable/datatable_order/Datatable_order"
+
+const List_order = () => {
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <Datatable_order />
+      </div>
+    </div>
+  )
+}
 
 export default List_order;
