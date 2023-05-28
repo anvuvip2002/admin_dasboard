b536b13f9ef60c6876154c621fdc417ec0021f97
Vippro67 <tuantu120502@gmail.com>
2023-05-28 23:35:20 +0700

    get cinema from backend

--- src/datatablesource_cinemas.js
+++ src/datatablesource_cinemas.js
@@ -1,123 +1,79 @@
+import axios from "axios";
 
+// export const cinemaColumns = [
+//     { field: "id", headerName: "ID", width: 70 },
+//     {
+//       field: "name",
+//       headerName: "Name",
+//       width: 200,
+//     },
+//     {
+//       field: "address",
+//       headerName: "Address",
+//       width: 150,
+//     },
+//       {
+//       field: "address_url",
+//       headerName: "Address URL",
+//       width: 200,
+//       renderCell: (params) => (
+//         <a href={params.value} target="_blank" rel="noreferrer">{params.value}</a>)
+//     },
+//     {
+//       field: "number_of_rooms",
+//       headerName: "Number rooms",
+//       width: 150,
+//     },
+//     {
+//       field: "province",
+//       headerName: "Province",
+//       width: 150,
+//     },
+    
+//     {
+//       field: "showtimes",
+//       headerName: "Show time",
+//       width: 150,
+//     },
+//     {
+//       field: "isActive",
+//       headerName: "Status",
+//       width: 160,
+//       renderCell: (params) => {
+//         return (
+//           <div className={params.row.isActive ? "active" : "passive" }>{params.row.isActive ? "active" : "passive" }</div>
+//         );
+//       },
+//     },
+//   ];
 
 export const cinemaColumns = [
-    { field: "id", headerName: "ID", width: 70 },
-    {
-      field: "name",
-      headerName: "Name",
-      width: 200,
-    },
-    {
-      field: "address",
-      headerName: "Address",
-      width: 150,
-    },
-      {
-      field: "address_url",
-      headerName: "Address URL",
-      width: 200,
-      renderCell: (params) => (
-        <a href={params.value} target="_blank" rel="noreferrer">{params.value}</a>)
-    },
-    {
-      field: "province",
-      headerName: "Province",
-      width: 150,
-    },
-    {
-      field: "number_of_rooms",
-      headerName: "Number rooms",
-      width: 150,
-    },
-    {
-      field: "showtimes",
-      headerName: "Show time",
-      width: 150,
-    },
-    {
-      field: "isActive",
-      headerName: "Status",
-      width: 160,
-      renderCell: (params) => {
-        return (
-          <div className={params.row.isActive ? "active" : "passive" }>{params.row.isActive ? "active" : "passive" }</div>
-        );
-      },
-    },
-  ];
-  export const cinemaRows = [
-    {
-      "id": 1,
-      "name": "CGV ABC",
-      "address": "KTX Khu A DHQG HCM",
-      "address_url": "https://map.google.com",
-      "province": "Dix An, Binh Duong",
-      "number_of_rooms": 9,
-      "showtimes": ["20:30"],
-      "isActive": true
-    },
-    {
-      "id": 2,
-      "name": "CGV XYZ",
-      "address": "123 Main St",
-      "address_url": "https://maps.google.com",
-      "province": "Some Province",
-      "number_of_rooms": 5,
-      "showtimes": ["12:30", "15:30", "18:30", "21:30"],
-      "isActive": true
-    },
-    {
-      "id": 3,
-      "name": "AMC Theatres",
-      "address": "1234 Elm St",
-      "address_url": "https://maps.amctheatres.com",
-      "province": "Some Other Province",
-      "number_of_rooms": 12,
-      "showtimes": ["13:00", "16:00", "19:00", "22:00"],
-      "isActive": false
-    },
-    {
-      "id": 4,
-      "name": "Regal Cinemas",
-      "address": "456 Broadway Ave",
-      "address_url": "https://maps.regalcinemas.com",
-      "province": "Some State",
-      "number_of_rooms": 8,
-      "showtimes": ["14:00", "17:00", "20:00", "23:00"],
-      "isActive": true
-    },
-    {
-      "id": 5,
-      "name": "Cineplex",
-      "address": "789 Maple St",
-      "address_url": "https://maps.cineplex.com",
-      "province": "Some Other State",
-      "number_of_rooms": 10,
-      "showtimes": ["11:00", "14:00", "17:00", "20:00", "23:00"],
-      "isActive": true
-    },
-    {
-      "id": 6,
-      "name": "MegaPlex Theatres",
-      "address": "1010 Oak St",
-      "address_url": "https://maps.megaplextheatres.com",
-      "province": "Another Province",
-      "number_of_rooms": 15,
-      "showtimes": ["12:00", "15:00", "18:00", "21:00"],
-      "isActive": true
-    },
-    {
-      "id": 7,
-      "name": "Cinema City",
-      "address": "1111 Pine St",
-      "address_url": "https://maps.cinemacity.com",
-      "province": "Another State",
-      "number_of_rooms": 7,
-      "showtimes": ["13:00", "16:00", "19:00", "22:00"],
-      "isActive": false
-    }
-  ];
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "name", headerName: "Name", width: 250 },
+  { field: "address", headerName: "Address", width: 250 },
+  {
+    field: "address_url",
+    headerName: "Address URL",
+    width: 350,
+    renderCell: (params) => (
+      <a href={params.value} target="_blank" rel="noreferrer">
+        {params.value}
+      </a>
+    ),
+  },
+  { field: "number_of_rooms", headerName: "Number of Rooms", width: 150 }
+];
   
+  export let cinemaRows = [];  
+const fetchCinemaData = () => {
+  axios.get("http://20.214.254.141:3000/cinema")
+    .then(response => {
+      cinemaRows = response.data;
+      console.log(cinemaRows);
+    })
+    .catch(error => {
+      console.error('Error fetching cinema data:', error);
+    });
+};
 
-  
-  
+fetchCinemaData();
--- src/pages/cinema/EditCinemas.jsx
+++ src/pages/cinema/EditCinemas.jsx
@@ -10,47 +10,43 @@ const EditCinemas = () => {
   const [data, setData] = useState(cinemaRows);
   const { id } = useParams();
   const cinema = data.find((item) => item.id == id);
-  return (  
+  return (
     <div className="list">
-      <Sidebar/>
+      <Sidebar />
       <div className="listContainer">
-        <Navbar/>
+        <Navbar />
         <form action="/cinemas/edit" method="POST">
-        <table>
-          <tr>
-            <td><label for="name">Name:</label></td>
-            <td><input type="text" id="name" name="name" placeholder={cinema.name}/></td>
-          </tr>
-          <tr>
-            <td><label for="address">Address:</label></td>
-            <td><input type="text" id="address" name="address" placeholder={cinema.address}/></td>
-          </tr>
-          <tr>
-            <td><label for="address_url">Address URL:</label></td>
-            <td><input type="url" id="address_url" name="address_url" placeholder={cinema.address_url}/></td>
-          </tr>
-          <tr>
-            <td><label for="province">Province:</label></td>
-            <td><input type="text" id="province" name="province" placeholder={cinema.province}/></td>
-          </tr>
-          <tr>
-            <td><label for="number_of_rooms">Number of Rooms:</label></td>
-            <td><input type="number" id="number_of_rooms" name="number_of_rooms" placeholder={cinema.number_of_rooms}/></td>
-          </tr>
-          <tr>
-            <td><label for="showtimes">Showtimes:</label></td>
+          <table>
+            <tr>
+              <td><label for="name">Name:</label></td>
+              <td><input type="text" id="name" name="name" placeholder={cinema.name} /></td>
+            </tr>
+            <tr>
+              <td><label for="address">Address:</label></td>
+              <td><input type="text" id="address" name="address" placeholder={cinema.address} /></td>
+            </tr>
+            <tr>
+              <td><label for="address_url">Address URL:</label></td>
+              <td><input type="url" id="address_url" name="address_url" placeholder={cinema.address_url} /></td>
+            </tr>
+            <tr>
+              <td><label for="province">Province:</label></td>
+              <td><input type="text" id="province" name="province" placeholder={cinema.province.name} /></td>
+            </tr>
+            <tr>
+              <td><label for="number_of_rooms">Number of Rooms:</label></td>
+              <td><input type="number" id="number_of_rooms" name="number_of_rooms" placeholder={cinema.number_of_rooms} /></td>
+            </tr>
+            <tr>
+              <td colspan="2"><input type="submit" value="Save Changes" /></td>
+            </tr>
+          </table>
+        </form>
 
-          </tr>
-          <tr>
-            <td colspan="2"><input type="submit" value="Save Changes" /></td>
-          </tr>
-        </table>
-</form>
 
-        
       </div>
     </div>
-    
+
   );
 };
 
